0fd7f8f012efedaa4432a7d86b5e9ab07aa22061
GYUTAEKIM <kkt219a@gmail.com>
2021-12-23 20:54:03 +0900

    feat: 예외에 사용될 메시지 정의
    - 예외들에서 사용될 메시지를 정의한다.

---
+++ src/main/java/pairmatching/constant/Notification.java
@@ -0,0 +1,10 @@
+package pairmatching.constant;
+
+public enum Notification {
+	CREW_NAME_DUPLICATE("[ERROR] 크루들 간의 이름은 중복될 수 없습니다.");
+	private final String message;
+
+	Notification(String message) {
+		this.message = message;
+	}
+}
