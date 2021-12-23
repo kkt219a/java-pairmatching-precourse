627ab6f5fccfbb39ce2b614e0195d8ca528a1523
GYUTAEKIM <kkt219a@gmail.com>
2021-12-23 21:43:25 +0900

    feat: 존재하는 미션, 페어 매칭/조회 출력 기능 구현
    - 존재하는 미션들을 나타낼 수 있도록 구현한다
    - 페어 매칭/조회 출력 기능을 구현한다.

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,10 @@
 package pairmatching;
 
+import pairmatching.processor.OutputProcessor;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+        new OutputProcessor().printPairInterface();
     }
 }
--- src/main/java/pairmatching/processor/OutputProcessor.java
+++ src/main/java/pairmatching/processor/OutputProcessor.java
@@ -6,12 +6,39 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import pairmatching.domain.Course;
+import pairmatching.domain.Level;
+import pairmatching.domain.Mission;
 
 public class OutputProcessor {
-	public String getMissionInformation() {
+	private static final String MISSION_OUTPUT = "미션: ";
+	private static final String COURSE_OUTPUT = "과정: ";
+	private static final String DELIMITER = "#############################################";
+	private static final String SELECT_INFORMATION = "과정, 레벨, 미션을 선택하세요.";
+	private static final String SELECT_EXAMPLE = "ex) 백엔드, 레벨1, 자동차경주";
+
+	public void printPairInterface() {
+		System.out.println(DELIMITER);
+		printCourseInformation();
+		printMissionInformation();
+		System.out.println(DELIMITER);
+		System.out.println(SELECT_INFORMATION);
+		System.out.println(SELECT_EXAMPLE);
+	}
+
+	private void printCourseInformation() {
+		System.out.print(COURSE_OUTPUT);
 		List<String> titles = Arrays.stream(Course.values())
 			.map(Course::getTitle)
 			.collect(Collectors.toList());
-		return String.join(" | ",titles);
+		System.out.println(String.join(" | ",titles));
+	}
+
+	private void printMissionInformation() {
+		System.out.println(MISSION_OUTPUT);
+		for (Level level : Level.values()) {
+			System.out.print("  - " + level.getTitle() + ": ");
+			List<String> titles = Mission.getMissionTitleByLevel(level);
+			System.out.println(String.join(" | ", titles));
+		}
 	}
 }
