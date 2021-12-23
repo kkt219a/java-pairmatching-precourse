7953d8c9d76969c9a93350fd91690d2758f05964
GYUTAEKIM <kkt219a@gmail.com>
2021-12-23 22:30:03 +0900

    feat: 동일한 페어 존재 여부 확인
    - 동일한 페어가 존재하는지 확인하기 위해 equals를 재정의한다.

--- src/main/java/pairmatching/domain/pair/Pair.java
+++ src/main/java/pairmatching/domain/pair/Pair.java
@@ -1,5 +1,7 @@
 package pairmatching.domain.pair;
 
+import java.util.Objects;
+
 import pairmatching.domain.Course;
 import pairmatching.domain.Level;
 import pairmatching.domain.Mission;
@@ -17,5 +19,20 @@ public class Pair {
 
 	public static Pair of(String course, String level, String mission) {
 		return new Pair(Course.of(course), Level.of(level), Mission.of(mission));
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Pair pair = (Pair)o;
+		return course == pair.course && level == pair.level && mission == pair.mission;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(course, level, mission);
 	}
 }
