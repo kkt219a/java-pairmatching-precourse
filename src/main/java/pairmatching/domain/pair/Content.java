f3f371df298116fa1dd00ce9eddda4702f851140
GYUTAEKIM <kkt219a@gmail.com>
2021-12-23 22:40:19 +0900

    refactor: pair의 상세내용을 content로 분리
    - pair에 들어간 과정, 레벨, 미션을 content로 분리한다.

---
+++ src/main/java/pairmatching/domain/pair/Content.java
@@ -0,0 +1,38 @@
+package pairmatching.domain.pair;
+
+import java.util.Objects;
+
+import pairmatching.domain.Course;
+import pairmatching.domain.Level;
+import pairmatching.domain.Mission;
+
+public class Content {
+	private final Course course;
+	private final Level level;
+	private final Mission mission;
+
+	private Content(Course course, Level level, Mission mission) {
+		this.course = course;
+		this.level = level;
+		this.mission = mission;
+	}
+
+	public static Content of(String course, String level, String mission) {
+		return new Content(Course.of(course), Level.of(level), Mission.of(mission));
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Content content = (Content)o;
+		return course == content.course && level == content.level && mission == content.mission;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(course, level, mission);
+	}
+}
--- src/main/java/pairmatching/domain/pair/Pair.java
+++ src/main/java/pairmatching/domain/pair/Pair.java
@@ -1,38 +1,15 @@
 package pairmatching.domain.pair;
 
-import java.util.Objects;
-
-import pairmatching.domain.Course;
-import pairmatching.domain.Level;
-import pairmatching.domain.Mission;
-
 public class Pair {
-	private final Course course;
-	private final Level level;
-	private final Mission mission;
+	private final Content content;
 
-	private Pair(Course course, Level level, Mission mission) {
-		this.course = course;
-		this.level = level;
-		this.mission = mission;
+	private Pair(Content content) {
+		this.content = content;
 	}
 
 	public static Pair of(String course, String level, String mission) {
-		return new Pair(Course.of(course), Level.of(level), Mission.of(mission));
+		Content content = Content.of(course, level, mission);
+		return new Pair(content);
 	}
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Pair pair = (Pair)o;
-		return course == pair.course && level == pair.level && mission == pair.mission;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(course, level, mission);
-	}
 }
